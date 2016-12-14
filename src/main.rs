fb446c6a4369fd2db20d36d8ca46fb211f86d943
Michael Walker <mike@barrucadu.co.uk>
2016-12-14 16:51:02 +0000

    In the beginning there was darkness.

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//! This is the entry point of the game.
+
+// Turn on some code quality linting
+#![warn(missing_copy_implementations, missing_debug_implementations, missing_docs, trivial_casts,
+        trivial_numeric_casts, unused_extern_crates, unused_import_braces, unused_qualifications,
+        unused_results)]
+
+fn main() {
+    println!("Welcome to Rogue Mayor!");
+}
