67194cf5b8edcb8bce8bf2978f869cb9f1970090
Michael Walker <mike@barrucadu.co.uk>
2016-12-18 16:11:26 +0000

    Allow window resizing.

--- src/ui/sdlui.rs
+++ src/ui/sdlui.rs
@@ -6,7 +6,7 @@ use grid::*;
 use mobiles::*;
 use sdl2;
 use sdl2::{EventPump, Sdl, VideoSubsystem};
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
@@ -27,13 +27,13 @@ const FONT_PATH: &'static str = "FSEX300.ttf";
 const FONT_SIZE: u16 = 12;
 
 // Size of the visible viewport, in grid cells.
-const VIEWPORT_CELL_HEIGHT: u32 = 50;
-const VIEWPORT_CELL_WIDTH: u32 = 100;
+const DEFAULT_VIEWPORT_CELL_HEIGHT: u32 = 50;
+const DEFAULT_VIEWPORT_CELL_WIDTH: u32 = 100;
 
 // Everything is done in terms of rows and columns, which are made of
 // fixed-size cells.
-const CELL_PIXEL_HEIGHT: u32 = 16;
-const CELL_PIXEL_WIDTH: u32 = 16;
+const DEFAULT_CELL_PIXEL_HEIGHT: u32 = 16;
+const DEFAULT_CELL_PIXEL_WIDTH: u32 = 16;
 
 // Thickness of a border, in cells.
 const BORDER_THICKNESS: u32 = 1;
@@ -44,17 +44,12 @@ const LOG_ENTRIES_VISIBLE: u32 = 7;
 // Number of cells to "overshoot" the cursor by when scrolling.
 const SCROLL_OVERSHOOT: usize = 25;
 
-// Some helpful derived stuff
-const SCREEN_WIDTH: u32 = CELL_PIXEL_WIDTH * (VIEWPORT_CELL_WIDTH + BORDER_THICKNESS * 2);
-const SCREEN_HEIGHT: u32 = CELL_PIXEL_HEIGHT *
-                           (LOG_ENTRIES_VISIBLE + BORDER_THICKNESS * 3 + VIEWPORT_CELL_HEIGHT);
-
 /// A user interface using SDL2.
 #[allow(missing_debug_implementations,missing_copy_implementations)]
 pub struct SdlUI {
+    /// The screen configuration.
+    screen: Screen,
     /// The top-left of the viewport.
-    viewport: Viewport,
-    /// The main interface to SDL.
     context: Sdl,
     /// The event pump: used to wait for and gather input events.
     events: EventPump,
@@ -90,8 +85,8 @@ enum Style {
 impl UI for SdlUI {
     fn initial_cursor() -> Point {
         Point {
-            x: VIEWPORT_CELL_WIDTH as usize / 2,
-            y: VIEWPORT_CELL_HEIGHT as usize / 2,
+            x: DEFAULT_VIEWPORT_CELL_WIDTH as usize / 2,
+            y: DEFAULT_VIEWPORT_CELL_HEIGHT as usize / 2,
         }
     }
 
@@ -118,7 +113,8 @@ impl UI for SdlUI {
 
         // The window frame
         render_border(&mut self.renderer,
-                      Rect::new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+                      self.screen,
+                      Rect::new(0, 0, self.screen.pixel_width(), self.screen.pixel_height()));
 
         // Finally, display everything.
         self.renderer.present();
@@ -148,6 +144,20 @@ impl UI for SdlUI {
             keydown!(LAlt) | keydown!(RAlt) => flag_set!(is_scrolling),
             keyup!(LAlt) | keyup!(RAlt) => flag_unset!(is_scrolling),
 
+            // Window
+            Event::Window { win_event: WindowEvent::Resized(w, h), .. } |
+            Event::Window { win_event: WindowEvent::SizeChanged(w, h), .. } => {
+                let cell_width = w as u32 / self.screen.cell_pixel_width;
+                let cell_height = h as u32 / self.screen.cell_pixel_height;
+
+                // Shrink the rendering area.
+                self.screen.viewport_width = cell_width.saturating_sub(2 * BORDER_THICKNESS);
+                self.screen.viewport_height =
+                    cell_height.saturating_sub(3 * BORDER_THICKNESS + LOG_ENTRIES_VISIBLE);
+
+                Command::Render
+            }
+
             // Building
             keydown!(Return) => Command::BuildTemplate,
 
@@ -155,17 +165,18 @@ impl UI for SdlUI {
             Event::MouseButtonDown { x, y, mouse_btn: MouseButton::Left, .. } => {
                 self.is_mousing = !self.is_mousing;
                 if self.is_mousing {
-                    Command::SetCursorTo(cursor_from_mouse(self.viewport, x, y))
+                    Command::SetCursorTo(cursor_from_mouse(self.screen, x, y))
                 } else {
                     Command::Render
                 }
             }
             Event::MouseMotion { x, y, .. } if self.is_mousing => {
-                Command::SetCursorTo(cursor_from_mouse(self.viewport, x, y))
+                Command::SetCursorTo(cursor_from_mouse(self.screen, x, y))
             }
             keydown!(Up) => {
                 if self.is_scrolling {
-                    self.viewport.top_left.y = self.viewport.top_left.y.saturating_sub(step);
+                    self.screen.viewport_top_left.y =
+                        self.screen.viewport_top_left.y.saturating_sub(step);
                     self.scroll_viewport(cursor, 0);
                     Command::Render
                 } else {
@@ -177,8 +188,8 @@ impl UI for SdlUI {
             }
             keydown!(Down) => {
                 if self.is_scrolling {
-                    self.viewport.top_left.y =
-                        cmp::min(HEIGHT, self.viewport.top_left.y.saturating_add(step));
+                    self.screen.viewport_top_left.y =
+                        cmp::min(HEIGHT, self.screen.viewport_top_left.y.saturating_add(step));
                     self.scroll_viewport(cursor, 0);
                     Command::Render
                 } else {
@@ -190,7 +201,8 @@ impl UI for SdlUI {
             }
             keydown!(Left) => {
                 if self.is_scrolling {
-                    self.viewport.top_left.x = self.viewport.top_left.x.saturating_sub(step);
+                    self.screen.viewport_top_left.x =
+                        self.screen.viewport_top_left.x.saturating_sub(step);
                     self.scroll_viewport(cursor, 0);
                     Command::Render
                 } else {
@@ -202,8 +214,8 @@ impl UI for SdlUI {
             }
             keydown!(Right) => {
                 if self.is_scrolling {
-                    self.viewport.top_left.x =
-                        cmp::min(WIDTH, self.viewport.top_left.x.saturating_add(step));
+                    self.screen.viewport_top_left.x =
+                        cmp::min(WIDTH, self.screen.viewport_top_left.x.saturating_add(step));
                     self.scroll_viewport(cursor, 0);
                     Command::Render
                 } else {
@@ -250,22 +262,27 @@ impl SdlUI {
             } )
         }
 
+        let screen = Screen {
+            cell_pixel_width: DEFAULT_CELL_PIXEL_WIDTH,
+            cell_pixel_height: DEFAULT_CELL_PIXEL_HEIGHT,
+            viewport_width: DEFAULT_VIEWPORT_CELL_WIDTH,
+            viewport_height: DEFAULT_VIEWPORT_CELL_HEIGHT,
+            viewport_top_left: Point { x: 0, y: 0 },
+        };
+
         let context = try!(sdl2::init());
         let events = try!(context.event_pump());
         let video = try!(context.video());
-        let window = ftry!(video.window("Rogue Mayor", SCREEN_WIDTH, SCREEN_HEIGHT)
+        let window = ftry!(video.window("Rogue Mayor", screen.pixel_width(), screen.pixel_height())
             .position_centered()
             .opengl()
+            .resizable()
             .build());
         let renderer = ftry!(window.renderer().build());
         let ttf = ftry!(sdl2::ttf::init());
 
         Ok(SdlUI {
-            viewport: Viewport {
-                top_left: Point { x: 0, y: 0 },
-                width: VIEWPORT_CELL_WIDTH as usize,
-                height: VIEWPORT_CELL_HEIGHT as usize,
-            },
+            screen: screen,
             context: context,
             events: events,
             video: video,
@@ -281,7 +298,7 @@ impl SdlUI {
 
     /// Render the cursor.
     fn render_cursor(&mut self, cursor: Point) {
-        if let Some(cursor_pos) = self.viewport.to_screenpos(cursor) {
+        if let Some(cursor_pos) = self.screen.to_screenpos(cursor) {
             let font = self.ttf.load_font(Path::new(FONT_PATH), FONT_SIZE).unwrap();
             let color = if self.is_mousing {
                 Color::RGB(150, 150, 255)
@@ -305,11 +322,12 @@ impl SdlUI {
         let color = Color::RGB(255, 255, 255);
         let mut done = 0;
         for msg in world.messages.iter().take(LOG_ENTRIES_VISIBLE as usize) {
-            let bbox = Rect::new((BORDER_THICKNESS * CELL_PIXEL_WIDTH) as i32,
+            let bbox = Rect::new((BORDER_THICKNESS * self.screen.cell_pixel_width) as i32,
                                  ((LOG_ENTRIES_VISIBLE - done - 1 + BORDER_THICKNESS) *
-                                  CELL_PIXEL_HEIGHT) as i32,
-                                 SCREEN_WIDTH,
-                                 CELL_PIXEL_HEIGHT);
+                                  self.screen
+                                     .cell_pixel_height) as i32,
+                                 self.screen.pixel_width(),
+                                 self.screen.cell_pixel_height);
             let surface = font.render(msg.msg.as_str()).blended(color).unwrap();
             let mut texture = self.renderer.create_texture_from_surface(&surface).unwrap();
             render_in(&mut self.renderer, &mut texture, bbox, false, true);
@@ -317,10 +335,11 @@ impl SdlUI {
         }
 
         render_border(&mut self.renderer,
+                      self.screen,
                       Rect::new(0,
                                 0,
-                                SCREEN_WIDTH,
-                                (LOG_ENTRIES_VISIBLE + 2) * CELL_PIXEL_HEIGHT));
+                                self.screen.pixel_width(),
+                                (LOG_ENTRIES_VISIBLE + 2) * self.screen.cell_pixel_height));
     }
 
     /// Render the world, with a heatmap overlay if enabled.
@@ -354,10 +373,10 @@ impl SdlUI {
         }
 
         // Render every cell.
-        let min_y = self.viewport.top_left.y;
-        let min_x = self.viewport.top_left.x;
-        for y in min_y..cmp::min(HEIGHT, min_y + self.viewport.height) {
-            for x in min_x..cmp::min(WIDTH, min_x + self.viewport.width) {
+        let min_y = self.screen.viewport_top_left.y;
+        let min_x = self.screen.viewport_top_left.x;
+        for y in min_y..cmp::min(HEIGHT, min_y + self.screen.viewport_height as usize) {
+            for x in min_x..cmp::min(WIDTH, min_x + self.screen.viewport_width as usize) {
                 let here = Point { x: x, y: y };
                 let color = if self.show_heatmap {
                     let val = map.at(here);
@@ -374,7 +393,7 @@ impl SdlUI {
                 };
                 render_cell(&mut self.renderer,
                             &font,
-                            self.viewport,
+                            self.screen,
                             here,
                             world.statics.at(here),
                             mobs.get(&here),
@@ -389,7 +408,7 @@ impl SdlUI {
         for (p, &(s, _)) in &tpl.components {
             render_cell(&mut self.renderer,
                         &font,
-                        self.viewport,
+                        self.screen,
                         p.offset(cursor),
                         Some(s),
                         None,
@@ -399,17 +418,21 @@ impl SdlUI {
 
     /// Scroll the viewport, with the given amount of overshoot, to fit the cursor.
     fn scroll_viewport(&mut self, cursor: Point, overshoot: usize) {
-        if cursor.x < self.viewport.top_left.x {
-            self.viewport.top_left.x = cursor.x.saturating_sub(overshoot);
-        } else if cursor.x >= self.viewport.top_left.x + self.viewport.width {
-            self.viewport.top_left.x = cmp::min(WIDTH - self.viewport.width,
-                                                cursor.x + overshoot - self.viewport.width)
+        if cursor.x < self.screen.viewport_top_left.x {
+            self.screen.viewport_top_left.x = cursor.x.saturating_sub(overshoot);
+        } else if cursor.x >=
+                  self.screen.viewport_top_left.x + self.screen.viewport_width as usize {
+            self.screen.viewport_top_left.x = cmp::min(WIDTH - self.screen.viewport_width as usize,
+                                                       cursor.x + overshoot -
+                                                       self.screen.viewport_width as usize)
         }
-        if cursor.y < self.viewport.top_left.y {
-            self.viewport.top_left.y = cursor.y.saturating_sub(overshoot);
-        } else if cursor.y >= self.viewport.top_left.y + self.viewport.height {
-            self.viewport.top_left.y = cmp::min(HEIGHT - self.viewport.height,
-                                                cursor.y + overshoot - self.viewport.height)
+        if cursor.y < self.screen.viewport_top_left.y {
+            self.screen.viewport_top_left.y = cursor.y.saturating_sub(overshoot);
+        } else if cursor.y >=
+                  self.screen.viewport_top_left.y + self.screen.viewport_height as usize {
+            self.screen.viewport_top_left.y =
+                cmp::min(HEIGHT - self.screen.viewport_height as usize,
+                         cursor.y + overshoot - self.screen.viewport_height as usize)
         }
     }
 }
@@ -427,9 +450,9 @@ fn next_heatmap(heatmap: (Style, MapTag)) -> (Style, MapTag) {
 }
 
 /// Render a border. This does not render over the area within the border.
-fn render_border(renderer: &mut Renderer<'static>, bbox: Rect) {
-    let hthickness = BORDER_THICKNESS * CELL_PIXEL_WIDTH;
-    let vthickness = BORDER_THICKNESS * CELL_PIXEL_HEIGHT;
+fn render_border(renderer: &mut Renderer<'static>, screen: Screen, bbox: Rect) {
+    let hthickness = BORDER_THICKNESS * screen.cell_pixel_width;
+    let vthickness = BORDER_THICKNESS * screen.cell_pixel_height;
 
     let x1 = bbox.x();
     let y1 = bbox.y();
@@ -447,12 +470,12 @@ fn render_border(renderer: &mut Renderer<'static>, bbox: Rect) {
 /// Render a cell.
 fn render_cell(renderer: &mut Renderer<'static>,
                font: &Font,
-               viewport: Viewport,
+               screen: Screen,
                cell: Point,
                s: Option<Static>,
                m: Option<&Mobile>,
                color: Option<Color>) {
-    if let Some(cell_screenpos) = viewport.to_screenpos(cell) {
+    if let Some(cell_screenpos) = screen.to_screenpos(cell) {
         let rect = cell_screenpos.rect();
 
         if let Some(c) = color {
@@ -543,53 +566,72 @@ fn render_in(renderer: &mut Renderer<'static>,
 }
 
 /// Get the cursor position from the mouse.
-fn cursor_from_mouse(viewport: Viewport, x: i32, y: i32) -> Point {
-    let cell_x = (x as u32 / CELL_PIXEL_WIDTH).saturating_sub(BORDER_THICKNESS);
-    let cell_y = (y as u32 / CELL_PIXEL_HEIGHT)
+fn cursor_from_mouse(screen: Screen, x: i32, y: i32) -> Point {
+    let cell_x = (x as u32 / screen.cell_pixel_width).saturating_sub(BORDER_THICKNESS);
+    let cell_y = (y as u32 / screen.cell_pixel_height)
         .saturating_sub(LOG_ENTRIES_VISIBLE + BORDER_THICKNESS * 2);
     Point {
-        x: cell_x as usize + viewport.top_left.x,
-        y: cell_y as usize + viewport.top_left.y,
+        x: cell_x as usize + screen.viewport_top_left.x,
+        y: cell_y as usize + screen.viewport_top_left.y,
     }
 }
 
-/// Screen positions, as CELL_PIXEL_WIDTHxCELL_PIXEL_HEIGHT boxes.
+/// The screen. Width and height are measured in cells.
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Screen {
+    cell_pixel_width: u32,
+    cell_pixel_height: u32,
+    viewport_width: u32,
+    viewport_height: u32,
+    viewport_top_left: Point,
+}
+
+impl Screen {
+    /// Width of the screen in pixels.
+    fn pixel_width(&self) -> u32 {
+        (self.viewport_width + BORDER_THICKNESS * 2) * self.cell_pixel_width
+    }
+
+    /// Height of the screen in pixels.
+    fn pixel_height(&self) -> u32 {
+        (self.viewport_height + LOG_ENTRIES_VISIBLE + BORDER_THICKNESS * 3) * self.cell_pixel_height
+    }
+
+    /// Turn a point in the world into a `ScreenPos`, if it's on screen.
+    fn to_screenpos(&self, p: Point) -> Option<ScreenPos> {
+        if p.x < self.viewport_top_left.x || p.y < self.viewport_top_left.y ||
+           p.x >= self.viewport_top_left.x + self.viewport_width as usize ||
+           p.y >= self.viewport_top_left.y + self.viewport_height as usize {
+            None
+        } else {
+            Some(ScreenPos {
+                x: (p.x - self.viewport_top_left.x) as u32,
+                y: (p.y - self.viewport_top_left.y) as u32,
+                cell_pixel_width: self.cell_pixel_width,
+                cell_pixel_height: self.cell_pixel_height,
+            })
+        }
+    }
+}
+
+/// Screen positions, as CELL_PIXEL_WIDTHxCELL_PIXEL_HEIGHT boxes. This is only valid until the
+/// screen is next resized.
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct ScreenPos {
     x: u32,
     y: u32,
+    // Taken from the `Screen` at creation time.
+    cell_pixel_width: u32,
+    cell_pixel_height: u32,
 }
 
 impl ScreenPos {
     /// Get the `Rect` that this position corresponds to.
     fn rect(&self) -> Rect {
-        Rect::new(((self.x + BORDER_THICKNESS) * CELL_PIXEL_WIDTH) as i32,
+        Rect::new(((self.x + BORDER_THICKNESS) * self.cell_pixel_width) as i32,
                   ((self.y + LOG_ENTRIES_VISIBLE + BORDER_THICKNESS * 2) *
-                   CELL_PIXEL_HEIGHT) as i32,
-                  CELL_PIXEL_WIDTH,
-                  CELL_PIXEL_HEIGHT)
-    }
-}
-
-/// The viewport.
-#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Viewport {
-    top_left: Point,
-    width: usize,
-    height: usize,
-}
-
-impl Viewport {
-    /// Turn a point in the world into a `ScrenPos`, if it's on screen.
-    fn to_screenpos(&self, p: Point) -> Option<ScreenPos> {
-        if p.x < self.top_left.x || p.y < self.top_left.y ||
-           p.x >= self.top_left.x + self.width || p.y >= self.top_left.y + self.height {
-            None
-        } else {
-            Some(ScreenPos {
-                x: (p.x - self.top_left.x) as u32,
-                y: (p.y - self.top_left.y) as u32,
-            })
-        }
+                   self.cell_pixel_height) as i32,
+                  self.cell_pixel_width,
+                  self.cell_pixel_height)
     }
 }
